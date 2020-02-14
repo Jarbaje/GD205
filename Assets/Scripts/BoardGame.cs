5ef4b42e531bd65dece4f4595764f20a9b09338f
Jarbaje <60495491+Jarbaje@users.noreply.github.com>
2020-02-13 20:17:45 -0500

    worked on hazards
    Created hazards in Unity using arrays in C#.

--- Assets/Scripts/BoardGame.cs
+++ Assets/Scripts/BoardGame.cs
@@ -6,23 +6,15 @@ public class BoardGame : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject c3,
-                      g1;
+    Vector3 initPos;
+    public Transform [] hazard;
+    
 
-    //These are all the interactive spaces that will be in the game. Currently only C3 is configured.
-    /*                a3,a7,
-                      b5,
-                      c1,c2,c3,c6,c8,
-                      e2,e3,e4,e5,e7,
-                      f1,f6,
-                      g1,g3,g4,g6,g8, //g1 is a token space.
-                      h1,h3,,h4,h8; // h1 is the goal and h3 is a token space.
-    */
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initPos = player.transform.position;
     }
 
     // Update is called once per frame
@@ -48,35 +40,27 @@ public class BoardGame : MonoBehaviour
             player.transform.position += new Vector3 (0,0,-1);
         }
 
-                if (Input.GetKeyDown(KeyCode.E) && player.transform.position.x == 7) // Up
+                if (Input.GetKeyDown(KeyCode.E)) // Up
         {
             player.transform.position += new Vector3 (0,1,0);
         }
 
-                if (Input.GetKeyDown(KeyCode.Q) && player.transform.position.y > 0) // Down
+                if (Input.GetKeyDown(KeyCode.Q)) // Down
         {
-           if (player.transform.position.z == 0)
-           {
                player.transform.position += new Vector3 (0,-1,0);
-           } 
         }
 
          if (Input.GetKeyDown(KeyCode.Z)) // Reset position
         {
             player.transform.position = new Vector3 (0,1,0);
-            player.transform.localScale = c3.transform.localScale;
         }
 
-        //Lowers the player one unit on the Y-Axis
-        if (player.transform.position.x == c3.transform.position.x && player.transform.position.z == c3.transform.position.z)
-        {
-            player.transform.position = new Vector3 (0,0,0);
-        }
+        for (int i = 0; i < hazard.Length; i++) {
 
-        //TBD
-        if (player.transform.position.x == g1.transform.position.x && player.transform.position.z == g1.transform.position.z)
-        {
-            player.transform.localScale = new Vector3 (1,2,1);
+            if (hazard[i].position == player.transform.position) 
+            {
+                player.transform.position = initPos;
+            }
         }
     }
 }
