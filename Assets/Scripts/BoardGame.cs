a62b75222df8aa1606f3bd4a89a6ca81ebc62312
Jarbaje <60495491+Jarbaje@users.noreply.github.com>
2020-02-26 22:16:29 -0500

    Completed first build
    This is a first build with a menu. Winning condition and pause/quit option added.

--- Assets/Scripts/BoardGame.cs
+++ Assets/Scripts/BoardGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoardGame : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class BoardGame : MonoBehaviour
     Vector3 initPos;
 
     //Arrays for hazards and tokens. Size adjusted in Unity
-    public GameObject [] hazard,
+    public GameObject [] hazard, hazard2, hazard3,
                         token;
 
     //x, y, and z positions of player, hazards, and tokens.
@@ -27,6 +28,10 @@ public class BoardGame : MonoBehaviour
                        hazardSound,
                        tokenSound,
                        resetPlayer;
+
+    Color playerColor;
+
+    public Transform[] goal;
     
 
 
@@ -54,6 +59,20 @@ public class BoardGame : MonoBehaviour
              zh[i] = Random.Range(0,8);
              hazard[i].transform.position = new Vector3 (xh[i], y, zh[i]);
         }
+        for (int i = 0; i < hazard2.Length; i++) 
+        {
+             xh[i] = Random.Range(0,8);
+             zh[i] = Random.Range(0,8);
+             hazard2[i].transform.position = new Vector3 (xh[i], y + 1, zh[i]);
+        }
+
+        for (int i = 0; i < hazard3.Length; i++) 
+        {
+             xh[i] = Random.Range(0,8);
+             zh[i] = Random.Range(0,8);
+             hazard3[i].transform.position = new Vector3 (xh[i], y -1, zh[i]);
+        }
+        
 
         // Gives a random value to each x and z object in the xt and zt array
         // Initializes every object in the token array with a random position for each object.
@@ -63,6 +82,8 @@ public class BoardGame : MonoBehaviour
              zt[i] = Random.Range(0,8);
              token[i].transform.position = new Vector3 (xt[i], y, zt[i]);
         }
+
+        playerColor = Color.black;
     }
 
     // Update is called once per frame
@@ -104,26 +125,51 @@ public class BoardGame : MonoBehaviour
                move.Play();
         }
 
-         if (Input.GetKeyDown(KeyCode.Z)) // Reset position
+         if (Input.GetKeyDown(KeyCode.Z)) // Reset game
         {
-            player.transform.position = initPos;
-            player.transform.localScale = new Vector3 (1,1,1);
-            resetPlayer.Play();
-            for (int i = 0; i < token.Length; i++)
-            {
-                if(token[i].transform.position.y < 1)
-                {
-                    token[i].transform.position += new Vector3 (0,3,0);
-                }
-            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // player.transform.position = initPos;
+            // player.transform.localScale = new Vector3 (1,1,1);
+            // resetPlayer.Play();
+            // for (int i = 0; i < token.Length; i++)
+            // {
+            //     if(token[i].transform.position.y < 1)
+            //     {
+            //         token[i].transform.position += new Vector3 (0,1,0);
+            //     }
+            // }
         }
 
         for (int i = 0; i < hazard.Length; i++) { //check for collision against in-game hazards
-
+          
             if (hazard[i].transform.position == player.transform.position) 
             {
                 player.transform.position = initPos;
                 hazardSound.Play();
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            }
+        }
+
+        for (int i = 0; i < hazard2.Length; i++) { //check for collision against in-game hazards
+          
+            if (hazard2[i].transform.position == player.transform.position) 
+            {
+                player.transform.position = initPos;
+                hazardSound.Play();
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            }
+        }
+
+        for (int i = 0; i < hazard3.Length; i++) { //check for collision against in-game hazards
+          
+            if (hazard3[i].transform.position == player.transform.position) 
+            {
+                player.transform.position = initPos;
+                hazardSound.Play();
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
             }
         }
 
@@ -132,10 +178,35 @@ public class BoardGame : MonoBehaviour
 
             if (token[i].transform.position == player.transform.position) 
             {
-                player.transform.localScale += new Vector3 (0,1,0);
-                token[i].transform.position += new Vector3 (0, -3, 0);
+                if (token[i].transform.position.y == 1)
+                {
+                playerColor.b += 0.20f;
+                player.GetComponent<Renderer>().material.color = playerColor;
+                token[i].transform.position += new Vector3 (0, 1, 0);
                 tokenSound.Play();
-                hazard[i].GetComponent<Renderer>().material.color = Color.black;
+                } else  if (token[i].transform.position.y == 2 && player.GetComponent<Renderer>().material.color.b == 1)
+                {
+                playerColor.r += 0.20f;
+                player.GetComponent<Renderer>().material.color = playerColor;
+                token[i].transform.position += new Vector3 (0, -2, 0);
+                tokenSound.Play();
+                } else  if (token[i].transform.position.y == 0 && player.GetComponent<Renderer>().material.color.r == 1)
+                {
+                playerColor.g += 0.20f;
+                player.GetComponent<Renderer>().material.color = playerColor;
+                token[i].transform.position += new Vector3 (0, -10, 0);
+                tokenSound.Play();
+                }
+
+            }
+        }
+
+        //Winning condition
+        for (var i = 0; i < goal.Length; i++)
+        {
+            if (player.transform.position == goal[i].position && player.GetComponent<Renderer>().material.color == goal[i].GetComponent<Renderer>().material.color)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
     }
